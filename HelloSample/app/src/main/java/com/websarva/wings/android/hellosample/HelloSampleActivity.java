3f0c7912679ce75650d849a5ae703aae239c7242
mizogami <tomochuri25@gmail.com>
2018-05-27 23:53:13 +0900

    Write Listener

--- HelloSample/app/src/main/java/com/websarva/wings/android/hellosample/HelloSampleActivity.java
+++ HelloSample/app/src/main/java/com/websarva/wings/android/hellosample/HelloSampleActivity.java
@@ -2,6 +2,11 @@ package com.websarva.wings.android.hellosample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import org.w3c.dom.Text;
 
 public class HelloSampleActivity extends AppCompatActivity {
 
@@ -9,5 +14,28 @@ public class HelloSampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_sample);
+
+        // Button オブジェクトを取得
+        Button btClick = findViewById(R.id.btClick);
+        // リスナクラスのインスタンスを生成
+        HelloListener listener = new HelloListener();
+        // 表示ボタンにリスナを設定
+        btClick.setOnClickListener(listener);
+    }
+
+    // ボタンクリックのリスナクラス
+    private class HelloListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            // Text オブジェクトを取得
+            EditText input = findViewById(R.id.etName);
+            // メッセージを表示する TextView オブジェクトを取得
+            TextView output = findViewById(R.id.tvOutput);
+            // 入力された名前文字列を取得
+            String inputStr = input.getText().toString();
+            // メッセージを表示
+            output.setText(inputStr + "さん、こんにちは！");
+        }
     }
 }
